0204eed164c384158a15b2e3f20172c88ae3c93e
Benjamin DuPont <ben@nebland.com>
2014-05-06 19:25:00 -0500

    remove sconsign.  add schema

---
+++ database/schema.4.sql
@@ -0,0 +1,24 @@
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE "frames" ("id" INTEGER PRIMARY KEY  NOT NULL ,"video_id" INTEGER,"num_objects" INTEGER,"original_image" BLOB,"bw_image" BLOB,"contour_image" BLOB,"reduced_image" BLOB,"number" INTEGER);
+CREATE UNIQUE INDEX "frames_video_number" on frames (video_id ASC, number ASC);
+CREATE TABLE "objects" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "frame_id" INTEGER,
+    "object_image" BLOB,
+    "contour" BLOB,
+    "x" INTEGER,
+    "y" INTEGER,
+    "height" INTEGER,
+    "width" INTEGER,
+    "number" INTEGER,
+    "label" TEXT,
+    "verified" INTEGER
+);
+CREATE UNIQUE INDEX "objects2_frame_number" on "objects" (frame_id ASC, number ASC);
+CREATE INDEX "objects2_label" on "objects" (label ASC);
+CREATE TABLE videos (
+    "id"INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "name" TEXT,
+    "rows" INTEGER,
+    "cols" INTEGER
+, "threshold1" INTEGER, "threshold2" INTEGER);
