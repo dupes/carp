673df4e1af0aa664ef10766a47664236627fdda6
Benjamin DuPont <ben@nebland.com>
2014-05-10 09:16:52 -0500

    implement FindCenterMean

--- cluster/Cluster/FindCenterMean.cpp
+++ cluster/Cluster/FindCenterMean.cpp
@@ -22,6 +22,25 @@ FindCenterMean::~FindCenterMean()
 Mat FindCenterMean::doReCenter(list<tObject*> objects, Distance *distance, int clusterID)
 {
 	Mat center;
+	list<tObject*>::iterator itr;
+
+	itr = objects.begin();
+
+	center = Mat((*itr)->object_image.cols, (*itr)->object_image.rows, CV_32FC1, 0.0);
+
+	int count = 0;
+
+	for (itr = objects.begin(); itr != objects.end(); itr++)
+	{
+		if ((*itr)->clusterID != clusterID)
+			continue;
+
+		add(center, (*itr)->object_image, center);
+
+		count++;
+	}
+
+	divide(count, center, center);
 
 	return center;
 }
